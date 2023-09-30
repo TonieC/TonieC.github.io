edc8cbf6cd98fe2368ee8f6f22ee06f495ca0409
Tonie <95538243+TonieC@users.noreply.github.com>
2023-09-30 23:49:33 +0800

    Changes

--- scr/script.js
+++ scr/script.js
@@ -1,1 +1,12 @@
-// Nothing to see here lol 
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll(".navbar a");
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            document.getElementById(targetId).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    }); 
+});

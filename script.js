ad82426e265d6bcd08aa26c21e22b1dee441ba23
Tonie <95538243+TonieC@users.noreply.github.com>
2024-02-14 04:33:48 +0800

    HAHAHA

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-var button = document.getElementById("button");
-  var button1 = document.getElementById("button1");
-  var image1 = document.getElementById("image1");
-  var image2 = document.getElementById("image2");
-
-  // Add click event listener to the first button
-  button.addEventListener("click", function() {
-    // Show Image 1 and hide Image 2
-    image1.style.display = "block";
-    image2.style.display = "none";
-  });
 
   // Add click event listener to the second button
   button1.addEventListener("click", function() {
     // Show Image 2 and hide Image 1
     image1.style.display = "none";
     image2.style.display = "block";
+  });
+  var button = document.getElementById("button");
+  var button1 = document.getElementById("button1");
+  var message = document.getElementById("message");
+
+  // Add click event listener to the first button
+  button.addEventListener("click", function() {
+    // Show the message and hide the first button
+    message.style.display = "block";
+    button.style.display = "none";
+  });
+
+  // Add click event listener to the second button
+  button1.addEventListener("click", function() {
+    // Show the message and hide the second button
+    message.style.display = "block";
+    button1.style.display = "none";
   });
